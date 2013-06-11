c538b5a0fd2a246b93d87b67781c51b3efc8801f
tosch <tosch@linux-6n34.site>
2013-06-11 15:52:32 +0200

    some amendmends

--- storage_stl_interface.hh
+++ storage_stl_interface.hh
@@ -15,6 +15,12 @@ void assign(std::vector<T>& target, const Storage1D<T,ST>& source);
 template<typename T1, typename T2, typename ST>
 void assign(Storage1D<T1,ST>& target, const Storage1D<T2,ST>& source);
 
+template<typename T1, typename T2, typename ST>
+void assign(Storage2D<T1,ST>& target, const Storage2D<T2,ST>& source);
+
+template<typename T1, typename T2, typename ST>
+void assign(Storage3D<T1,ST>& target, const Storage3D<T2,ST>& source);
+
 /************** implementation *************/
 
 
@@ -46,6 +52,24 @@ void assign(Storage1D<T1,ST>& target, const Storage1D<T2,ST>& source) {
   target.resize_dirty(source.size());
   for (uint k=0; k < source.size(); k++)
     target[k] = (T1) source[k];  
+}
+
+template<typename T1, typename T2, typename ST>
+void assign(Storage2D<T1,ST>& target, const Storage2D<T2,ST>& source) {
+
+  target.resize_dirty(source.xDim(),source.yDim());
+
+  for (uint k=0; k < source.size(); k++)
+    target.direct_access(k) = (T1) source.direct_access(k);
+}
+
+template<typename T1, typename T2, typename ST>
+void assign(Storage3D<T1,ST>& target, const Storage3D<T2,ST>& source) {
+
+  target.resize_dirty(source.xDim(),source.yDim(),source.zDim());
+
+  for (uint k=0; k < source.size(); k++)
+    target.direct_access(k) = (T1) source.direct_access(k);
 }
 
 #endif
